72da6d93f83ecb7f9c8121b94ed3fcb201186702
Dilhanx <dilhanx@outlook.com>
2017-09-05 12:54:53 +0530

    vcncvn

--- main.py
+++ main.py
@@ -8,27 +8,29 @@ import time
 import collections
 app = Flask(__name__)
 def connectdb():# Create connection to sql database
-  app.logger.info("Begining database connection")
-  file = open("dbc.txt", "r")
-  text = file.readline().split(",")
-  app.logger.info("File open")
-  # Asign values to  send
+  # app.logger.info("Begining database connection")
+  # file = open("dbc.txt", "r")
+  # text = file.readline().split(",")
+  # app.logger.info("File open")
+  # # Asign values to  send
 
-  # Asign database connection details
-  server = text[0]  # server = "kappaorbannedj.database.windows.net"
+  # # Asign database connection details
+  # server = text[0]  # server = "kappaorbannedj.database.windows.net"
 
-  database = text[1]   # database = "TwitchStats"
+  # database = text[1]   # database = "TwitchStats"
 
-  username = text[2] # username = "Dilhan@kappaorbannedj"
+  # username = text[2] # username = "Dilhan@kappaorbannedj"
 
-  password = text[3]   # password = "101Luminous101"
+  # password = text[3]   # password = "101Luminous101"
   
-  driver= text[4] # driver="{ODBC Driver 13 for SQL Server}"   
+  # driver= text[4] # driver="{ODBC Driver 13 for SQL Server}"   
   
  
-  # file.close()
-  app.logger.info("File close")
-  db = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+  # # file.close()
+  # app.logger.info("File close")
+  # db = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+  # 
+  
   return db
 @app.route('/banned')
 def banned():
@@ -58,11 +60,11 @@ def search():
   if row.cou==1:
     app.logger.info(request.form['streamer']+" is avaliable")
     db.close()
-    return "{\"status\": \"valid\"}"#Return valid if found
+    return "{\"status\": \"Found\"}"#Return valid if found
   else:
     app.logger.info(request.form['streamer']+" does not exist")
     db.close()
-    return "{\"status\": \"Invalid\"}"#Return valid if not found
+    return "{\"status\": \"Not found\"}"#Return valid if not found
 
 @app.route('/streamer/<streamername>')# Retrive streamer detials
 def streamer(streamername):
@@ -89,6 +91,7 @@ def streamer(streamername):
     d['bio'] = row.bio
     details.append(d)
     j = json.dumps(details)
+    j=(j[1:-1])
     db.close()
     app.logger.info(" Sending json message")
     return j
@@ -97,15 +100,16 @@ def streamer(streamername):
     
  
 
-@app.route('/streamer/<streamername>/comment')# Retrive comments about streamer
+@app.route('/streamer/<streamername>/comment')
 def streamercomment(streamername):
-  app.logger.info("Streamer Comments"+streamername) 
   try:
-    db = connectdb()
+      db = connectdb()
   except Exception as identifier:
-    app.logger.error(identifier)
-    return "{\"status\": \"No connection to database\"}"
-  app.logger.info("Connection establish")  
+      app.logger.error(identifier)
+      return "{\"status\": \"No connection to database\"}"
+  app.logger.info("Connection establish")
+  app.logger.info("Streamer Comments"+streamername) 
+  
   cursor = db.cursor()
   app.logger.info("Send sql statment") 
   cursor.execute("SELECT * From comment where streamer_name= '"+ streamername +"' ;") #Send sql statment to select all data of streamer
@@ -115,15 +119,39 @@ def streamercomment(streamername):
   app.logger.info( streamername +" is avaliable")
   for row in rows:
     d = collections.OrderedDict()
-    d['streamer_name'] = row.streamer_name
-    d['user_name'] = row.user_name
-    d['comment'] = row.comment
+    d["streamer_name"] = row.streamer_name
+    d["user_name"] = row.user_name
+    d["comment"] = row.comment
     details.append(d)
   j = json.dumps(details)
-  db.close()
-  app.logger.info(" Sending json message")
-  return j
 
+  app.logger.info(" Sending json message"+j)
+  return j
+  
+@app.route('/streamer/<streamername>/comment/update',methods=['POST'])
+def updatecomment(streamername):
+  try:
+      db = connectdb()
+  except Exception as identifier:
+      app.logger.error(identifier)
+      return "{\"status\": \"No connection to database\"}"
+  app.logger.info("Connection establish")  
+  if request.form['type']=='add':#Add comment
+      app.logger.info("Add"+request.form['streamername'])     
+      cursor = db.cursor()
+      app.logger.info("Send sql statment") 
+      cursor.execute("SELECT * insert into comment values('"+ request.form['streamername']+"','"+request.form['user']+"','"+ request.form['comment'] +"') ;")
+      db.commit()
+      db.close()
+      return "{\"status\": \"Add\"}"
+  elif request.form['type']=='delete':#Delete comment
+      app.logger.info("Delete"+streamername)     
+      cursor = db.cursor()
+      app.logger.info("Send sql statment") 
+      cursor.execute("DELETE FROM table_name WHERE streamer_name='"+request.form['streamername']+"',user_name="+request.form['user'] +"',comment="+request.form['comment']+"') ;")
+      db.commit()
+      db.close()
+      return "{\"status\": \"Deleted\"}"
 @app.route('/streamer/<streamername>/emote')# Retrive emotes used by streamer
 def streameremote(streamername):
   app.logger.info("Streamer Comments"+streamername) 
@@ -180,8 +208,7 @@ def register():
   app.logger.info("Connection establish")  
   cursor = db.cursor()
   app.logger.info("Send sql statment") 
-  cursor.execute("SELECT * From user_account where user_name= '"+request.form['username'] +"';") #Send sql statment to check if account is all ready made
-  row = cursor.fetchone()
+ 
   cursor.execute("SELECT count(*) As cou From user_account where user_name= '"+request.form['username'] +"';") #Send sql statment to check if account is all ready made  
   row = cursor.fetchone()
  
@@ -195,11 +222,35 @@ def register():
     db.commit()
     db.close()
     return "{\"status\": \"Account created\"}"  
+@app.route('/delete',methods=['POST']) # The delete method
+def delete():
+  try:
+     db = connectdb()
+  except Exception as identifier:
+    app.logger.error(identifier)
+    return "{\"status\": \"No connection to database\"}"
+  app.logger.info("Connection establish")  
+  cursor = db.cursor()
+  app.logger.info("Send sql statment") 
+ 
+  cursor.execute("SELECT count(*) As cou From user_account where user_name= '"+request.form['username'] +"';") #Send sql statment to check if account is  made  
+  row = cursor.fetchone()
+ 
+  if row.cou!=1:
+    db.close()
+    app.logger.info(request.form['username']+" allredy has an account")
+    return "{\"status\": \"Account not there\"}"
+  else:
+    app.logger.info(request.form['username']+" account deleted")
+    cursor.execute("Delete From user_account where user_name='"+request.form['username']+"'AND password='"+request.form['password']+"';")
+    db.commit()
+    db.close()
+    return "{\"status\": \"Account deleted\"}"  
 
 if __name__ == '__main__':
   #Create Logger
   formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s") #Set log message formate
-  handler = RotatingFileHandler("Log/"+time.strftime("%Y-%m-%d ")+".log", maxBytes=10000, backupCount=1) # Set log file
+  handler = RotatingFileHandler("Log/"+time.strftime("%Y-%m-%d ")+".log",  backupCount=1) # Set log file
   handler.setLevel(logging.INFO) 
   handler.setFormatter(formatter)
   app.logger.addHandler(handler) 
